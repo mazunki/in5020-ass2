3b33c4ba6c1a02019f8df19305137d579af28ced
Lise Ekhorn Johansen <liseej@uio.no>
2024-10-22 15:33:49 +0200

    naive

--- src/main/java/com/ass2/Client.java
+++ src/main/java/com/ass2/Client.java
@@ -200,8 +200,8 @@ public class Client {
 		debug("getting synced balance");
 
 		BigDecimal value;
-		// value = this.getNaiveSyncedBalance();
-		value = this.getOptimizedSyncedBalance();
+		value = this.getNaiveSyncedBalance();
+		//value = this.getOptimizedSyncedBalance();
 
 		System.out.println(value);
 		return value;
